17d33cab5061c43253cc5c1adfecfcfe49c9bf48
Mateo487 <150305375+Mateo487@users.noreply.github.com>
2024-02-14 13:59:22 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# StringInterpolationTemplate
+This template provides the basic structure including methods and tests to complete and submit the StringInterpolation assignment.  Follow the instructions below to complete the assignment.  
+
+## Assignment Instructions
+* Place your code into the StringInterpolationService.cs methods.
+   - Follow the included Number01() method as an example
+* Be sure your method returns your answer
+   - Note all methods return strings
+* Use the _date private variable for today's date ***(rather than DateTime.Now)***
+   - Note this is for the test runner
+
+## Requirements 
+### From today's date
+1. January 22, 2019 (right aligned in a 40 character field)
+2. 2019.01.22
+3. Day 22 of January, 2019
+4. Year: 2019, Month: 01, Day: 22
+5. Tuesday (10 spaces total, right aligned)
+6. 11:01 PM Tuesday (10 spaces total for each including the day-of-week, both right-aligned)
+7. h:11, m:01, s:27
+8. 2019.01.22.11.01.27
+
+### If you have PI (3.1415) ***(use `var pi = Math.PI;`)***
+
+ 9. Output as currency
+10. Output as right-aligned (10 spaces), number with 3 decimal places
+11. Output the year (2019) as a hexidecimal value.
+   
+## Links:
+
+[Standard Date and Time Format Strings](https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings)
---
+++ StringInterpolationTemplate/Services/StringInterpolationService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using StringInterpolationTemplate.Utils;
+
+namespace StringInterpolationTemplate.Services;
+
+public class StringInterpolationService : IStringInterpolationService
+{
+    private readonly ISystemDate _date;
+    private readonly ILogger<IStringInterpolationService> _logger;
+
+    public StringInterpolationService(ISystemDate date, ILogger<IStringInterpolationService> logger)
+    {
+        _date = date;
+        _logger = logger;
+        _logger.Log(LogLevel.Information, "Executing the StringInterpolationService");
+    }
+
+    // 1. January 22, 2019 (right aligned in a 40 character field)
+    public string Number01()
+    {
+        var date = _date.Now.ToString("MMMM dd, yyyy");
+        var answer = $"{date,40}";
+        Console.WriteLine(answer);
+
+        return answer;
+    }
+
+    // 2. 2019.01.22
+    public string Number02()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 3. Day 22 of January, 2019
+    public string Number03()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 4. Year: 2019, Month: 01, Day: 22
+    public string Number04()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 5.            Tuesday (10 spaces total, right aligned)
+    public string Number05()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 6.     11:01 PM             Tuesday (10 spaces total for each including the day-of-week, both right-aligned)
+    public string Number06()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 7. h:11, m:01, s:27
+    public string Number07()
+    {
+        throw new NotImplementedException();
+    }
+
+    // 8. 2019.01.22.11.01.27
+    public string Number08()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Output Math.PI as currency
+    public string Number09()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Output Math.PI as right-aligned (10 spaces), number with 3 decimal places
+    public string Number10()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Output the year (2019) as a hexidecimal value
+    public string Number11()
+    {
+        throw new NotImplementedException();
+    }
+}
